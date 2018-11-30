ca3e3396c78aca9a7f16686a7196ba8faf32ac39
takashiAg <crawd4274@gmail.com>
2018-11-30 15:56:48 +0900

    とりあえず

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// urlencodedとjsonは別々に初期化する
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
 app.get('/', function (req, res) {
     console.log(req.query);
     console.log(req.body);
     res.send('Done');
 });
+app.post('/', function (req, res) {
+    // console.log(req.query);
+    console.log(req.body.ref);
+    res.send('done');
+})
 app.listen(3000, function () {
 });
