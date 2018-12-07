c9ebf5701a81da8e3e456242e00e991cf8a9b790
takashiAg <crawd4274@gmail.com>
2018-12-07 13:01:02 +0900

    完成

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+const {exec} = require('child_process');
+const file = require('./file')
 
-// urlencodedとjsonは別々に初期化する
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -11,15 +11,42 @@ app.use(bodyParser.json());
 
 
 app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.post('/', async function (req, res) {
     console.log(req.query);
-    console.log(req.body);
+    console.log("body", req.body);
+    if (!"path" in req.body || !"branch" in req.body || !"key" in req.body) {
+        res.send('Bad request');
+        return;
+    }
+    let data = {"path": req.body.path, "branch": req.body.branch, "key": req.body.key}
+    await file.write(__dirname + '/data.json', JSON.stringify(data, null, '    '))
+
     res.send('Done');
 });
-app.post('/', function (req, res) {
-    // console.log(req.query);g
+app.post('/renew', async function (req, res) {
+    console.log(req.query);
+    if (!("ref" in req.body))
+        return;
     let branch = req.body.ref.match(/refs\/heads\/(.*)/)[1]
-    console.log(branch)
+    await git_pull();
     res.send('done');
 })
 app.listen(3000, function () {
 });
+
+async function git_pull() {
+    let {path, branch} = await file.read_json(__dirname + "/data.json")
+    let command = "cd " + path + " && git --git-dir=.git pull";
+
+    let {stdout, stderr} = await new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(error)
+                return
+            }
+            resolve({stdout: stdout, stderr: stderr});
+        });
+    })
+}
---
+++ file.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+
+const read = (path, opts = 'utf8') =>
+    new Promise((res, rej) => {
+        fs.readFile(path, opts, (err, data) => {
+            if (err) rej(err)
+            else res(data)
+        })
+    })
+
+const read_json = (path, opts = 'utf8') =>
+    new Promise((res, rej) => {
+        fs.readFile(path, opts, (err, data) => {
+            if (err) rej(err)
+            else res(JSON.parse(data))
+        })
+    })
+
+const write = (path, data, opts = 'utf8') =>
+    new Promise((res, rej) => {
+        fs.writeFile(path, data, opts, (err) => {
+            if (err) rej(err)
+            else res()
+        })
+    })
+
+module.exports = {
+    read,
+    write,
+    read_json
+}
